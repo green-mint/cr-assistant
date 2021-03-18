0b85c96a6c6a9ecea60270c84cfc2adae6f42efa
green-mint <saad.521509@gmail.com>
2021-03-18 22:42:23 +0500

    ahhhhhhh

--- main.js
+++ main.js
@@ -1,41 +1,9 @@
 const venom = require('venom-bot');
 const fs = require('fs');
 
-const fs = require('fs');
-const venom = require('venom-bot');
-
 venom
-  .create(
-    'sessionName',
-    (base64Qr, asciiQR, attempts, urlCode) => {
-      console.log(asciiQR); // Optional to log the QR in the terminal
-      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
-
-      if (matches.length !== 3) {
-        return new Error('Invalid input string');
-      }
-      response.type = matches[1];
-      response.data = new Buffer.from(matches[2], 'base64');
-
-      var imageBuffer = response;
-      require('fs').writeFile(
-        'out.png',
-        imageBuffer['data'],
-        'binary',
-        function (err) {
-          if (err != null) {
-            console.log(err);
-          }
-        }
-      );
-    },
-    undefined,
-    { logQR: false }
-  )
-  .then((client) => {
-    remind(client);
-  })
+  .create()
+  .then((client) => start(client))
   .catch((erro) => {
     console.log(erro);
   });
