2c813816000aa196f42f18a61da5581f53e9fe18
green-mint <saad.521509@gmail.com>
2021-03-18 21:50:54 +0500

    ahhhhhhh

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const venom = require('venom-bot');
 const fs = require('fs');
 
+const fs = require('fs');
+const venom = require('venom-bot');
+
 venom
-  .create()
-  .then((client) => remind(client))
+  .create(
+    'sessionName',
+    (base64Qr, asciiQR, attempts, urlCode) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'out.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => {
+    remind(client);
+  })
   .catch((erro) => {
     console.log(erro);
   });
@@ -47,3 +79,4 @@ function getDeadlines() {
   }
   return message;
 }
+
