c38858475ed0f44e2e7764f46715ab9adf3a4c55
green-mint <saad.521509@gmail.com>
2021-03-18 17:25:09 +0500

    test version 1

---
+++ main.js
@@ -0,0 +1,49 @@
+const venom = require('venom-bot');
+const fs = require('fs');
+
+venom
+  .create()
+  .then((client) => remind(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+
+
+async function remind(client) {
+    (async function loop() {
+        let now = new Date();
+        let delay;
+        console.log(now.getHours())
+        if (now.getHours() === 17) {
+            delay = 8.54e+7 // approximately 0.9 day
+            let message = getDeadlines();
+            await client
+              .sendText('923165192717-1558982529@g.us', message)
+              .then((result) => {
+                console.log('Result: ', result); //return object success
+              })
+              .catch((erro) => {
+                console.error('Error when sending: ', erro); //return object error
+              });
+        } else {
+          now = new Date();                  // allow for time passing
+          delay = 60000 - (now % 60000); // exact ms to next minute interval
+        }
+        setTimeout(loop, delay);
+    })();
+}
+
+function getDeadlines() {
+  let rawdata = fs.readFileSync('tasks.json');
+  let message = ``;
+  deadlines = JSON.parse(rawdata);
+
+  for (var subject in deadlines) {
+    for (var task in deadlines[subject]) {
+      message = message.concat(`The deadline for ${subject} ${task} is ${deadlines[subject][task]}\n`)
+    }
+  }
+  return message;
+}
+
